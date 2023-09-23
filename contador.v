b42b5696a8e43d5b79436e9731fbe9401db4c1c1
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-09-23 14:13:38 -0300

    feat: add contador

---
+++ contador.v
@@ -0,0 +1,46 @@
+/*
+	Contador de 0 até 4
+	Entrada: clock
+	Saída: S
+*/
+
+module contador(clock, S);
+	input clock;
+	output reg [2:0] S;
+
+	initial begin
+		S = 3'b000;
+	end
+
+	always @(posedge clock)
+	begin
+		if (S == 3'b100)
+			S <= 3'b000;
+		else
+			S <= S + 1;
+	end
+
+endmodule
+
+
+module TB_contador;
+	reg clock;
+	wire [2:0] S;
+
+	contador contador(clock, S);
+
+	initial begin
+		clock = 0; #10; // S = 000
+		clock = 1; #10; // S = 001
+		clock = 0; #10; // S = 001
+		clock = 1; #10; // S = 010
+		clock = 0; #10; // S = 010
+		clock = 1; #10; // S = 011
+		clock = 0; #10; // S = 011
+		clock = 1; #10; // S = 100
+		clock = 0; #10; // S = 100
+		clock = 1; #10; // S = 000
+		clock = 0; #10; // S = 000
+		clock = 1; #10; // S = 001
+	end
+endmodule
