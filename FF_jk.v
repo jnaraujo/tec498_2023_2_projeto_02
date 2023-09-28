989f98b38e6b42f027d5858c0a257b4ae6510904
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-09-28 16:07:44 -0300

    improved ff jk

--- FF_jk.v
+++ FF_jk.v
@@ -13,15 +13,14 @@ module FF_jk(
 
   always @(posedge clock or posedge reset) begin
     if (reset) begin
-      q <= 1'b0;
+      q = 1'b0;
     end else begin
-      if (j & ~k) begin
-        q <= 1'b1;
-      end else if (~j & k) begin
-        q <= 1'b0;
-      end else if (j & k) begin
-        q <= ~q;
-      end
+      case ({j, k})
+        2'b00: q = q;
+        2'b01: q = 1'b0;
+        2'b10: q = 1'b1;
+        2'b11: q = ~q;
+      endcase
     end
   end
 endmodule
