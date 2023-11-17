03cb26ec57cb0e76f1470faedb7a567d86a9456a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2023-11-17 18:02:12 -0300

    🐛 fix: decodificador coluna

--- decod_col.v
+++ decod_col.v
@@ -1,60 +1,59 @@
 module decod_col(
-  enable,
   sel,
   a, b, c, d, e, f, g, dp
 );
-  input enable;
   input [2:0] sel;
 
   output a, b, c, d, e, f, g, dp;
 
-  wire w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12;
+  wire w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10, w11, w12, w13;
 
   // NOT a OR (b AND c) OR d
   and and0(w0, sel[2], sel[1]);
-  or or0(a, ~enable, w0, sel[0]);
+  or or0(a, w0, sel[0]);
 
   // NOT a OR b OR (c AND NOT d) OR ( NOT c AND d)
   and and1(w1, sel[1], ~sel[0]);
   and and2(w2, ~sel[1], sel[0]);
-  or or1(b, ~enable, sel[2], w1, w2);
+  or or1(b, sel[2], w1, w2);
 
   // NOT a OR b OR (c AND NOT d)
   and and3(w3, sel[1], ~sel[0]);
-  or or2(c, ~enable, sel[2], w3);
+  or or2(c, sel[2], w3);
 
   // NOT a OR (b AND c) OR (b AND d) OR ( NOT b AND NOT c AND NOT d)
   and and4(w4, sel[2], sel[1]);
   and and5(w5, sel[2], sel[0]);
   and and6(w6, ~sel[2], ~sel[1], ~sel[0]);
-  or or3(d, ~enable, w4, w5, w6);
+  or or3(d, w4, w5, w6);
 
   // NOT a OR (b AND c) OR (b AND d)
   and and7(w7, sel[2], sel[1]);
   and and8(w8, sel[2], sel[0]);
-  or or4(e, ~enable, w7, w8);
+  or or4(e, w7, w8);
 
   // NOT a OR (b AND c) OR (b AND d) OR (c AND d)
   and and9(w9, sel[2], sel[1]);
   and and10(w10, sel[2], sel[0]);
   and and11(w11, sel[1], sel[0]);
-  or or5(f, ~enable, w9, w10, w11);
+  or or5(f, w9, w10, w11);
 
-  // NOT a OR (b AND d) OR c
+  // NOT a OR (b AND d) OR (c AND NOT d)
   and and12(w12, sel[2], sel[0]);
-  or or6(g, ~enable, w12, sel[1]);
+  and and13(w13, sel[1], ~sel[0]);
+  or or6(g, w12, w13);
 
   // 1
   assign dp = 1'b1;
 endmodule
 
 module TB_decod_col();
-  reg enable;
+reg enable;
   reg [2:0] sel;
 
   wire a, b, c, d, e, f, g, dp;
 
-  decod_col decod_col(enable, sel, a, b, c, d, e, f, g, dp);
+  decod_col decod_col(sel, a, b, c, d, e, f, g, dp);
 
   initial begin
     enable = 1'b1; sel = 3'b000; #10; // 0	0	0	1	0	0	0	1
